4fc80c8cf7f9f8c6f6015877b12a7cd3c25a94c3
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2026-06-30 10:23:33 -0400

    Added code to App.js

---
+++ my-react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <WeatherSearch />
+    </div>
+  );
+}
